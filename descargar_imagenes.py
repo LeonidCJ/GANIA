a8af6adaed3aeb61da9226fb9090c0b453a779c3
Diego Matías <159220471+DiegoKeiO@users.noreply.github.com>
2025-07-01 21:09:48 -0500

    controles

--- descargar_imagenes.py
+++ descargar_imagenes.py
@@ -1,11 +1,27 @@
 from bing_image_downloader import downloader
+import os
+import zipfile
+import urllib.request
 
-
-
-downloader.download("City photos", limit=100, output_dir="./datasets/arte_b", adult_filter_off=True)
+def descargar_apple2orange(destino='./datasets'):
+    url = "https://efrosgans.eecs.berkeley.edu/cyclegan/datasets/apple2orange.zip"
+    zip_path = os.path.join(destino, "apple2orange.zip")
+    os.makedirs(destino, exist_ok=True)
+    print("Descargando apple2orange…")
+    urllib.request.urlretrieve(url, zip_path)
+    print("Descomprimiendo…")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(destino)
+    print("Listo:")
+    print(f"- {destino}/apple2orange/trainA (manzanas)")
+    print(f"- {destino}/apple2orange/trainB (naranjas)")
 
 
 """
+downloader.download("City photos", limit=100, output_dir="./datasets/arte_b", adult_filter_off=True)
+
+
+
 # CycleGAN B
 downloader.download("forest paintings", limit=20, output_dir="./datasets/arte_b", adult_filter_off=True)
 
@@ -22,4 +38,3 @@ downloader.download("Vincent van Gogh paintings", limit=100, output_dir="./datas
 
 """
 
-print("Descarga completada.")
